8f755950a82e7c706ba6b17197c0278a548c1774
Hemanthdeva <33149969+Hemanthdeva@users.noreply.github.com>
2019-03-22 15:31:06 +0530

    Update ExampleUnitTest.java

--- app/src/test/java/com/practice/myapplication/ExampleUnitTest.java
+++ app/src/test/java/com/practice/myapplication/ExampleUnitTest.java
@@ -16,6 +16,7 @@ import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
+import org.testng.annotations.Test;
 
 public class ExampleUnitTest {
 
